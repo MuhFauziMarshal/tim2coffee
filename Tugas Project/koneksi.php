9cf73c07f68d0afcd735f8bde854c6c80edd974a
afrizal0 <afriizaalll@gmail.com>
2024-05-31 18:53:43 +0800

    fix: login bug

--- Tugas Project/koneksi.php
+++ Tugas Project/koneksi.php
@@ -1,6 +1,6 @@
-<? 
+<?php
     $conn = mysqli_connect("localhost","root","","tim2coffee");
-    if($conn){
+    if(!$conn){
         echo "Gagal Koneksi!";
     }
 ?>
--- Tugas Project/proses_login.php
+++ Tugas Project/proses_login.php
@@ -1,17 +1,18 @@
 <?php
-include "koneksi.php";
-session_start();
+   include "koneksi.php";
+   session_start();
 
-$username = (isset($_POST['username'])) ? htmlentities($_POST['username']) : "";
-$password = (isset($_POST['password'])) ? htmlentities($_POST['password']) : "";
-if (!empty ($_POST['submit_validate'])) {
-   $query = mysqli_query($conn,"SELECT *FROM tb_user WHERE username = '$username' && password = '$password'");
-   $hasil = mysqli_fetch_array($query);
-   if ($hasil) {
-      header('Location: index.php');
-      exit();
-   } else {
-      echo "Username dan Password Salah";
+   $username = (isset($_POST['username'])) ? htmlentities($_POST['username']) : "";
+   $password = (isset($_POST['password'])) ? htmlentities($_POST['password']) : "";
+   if (!empty ($_POST['submit_validate'])) {
+      $query = mysqli_query($conn,"SELECT * FROM tb_user WHERE username = '$username' && password = '$password'");
+      $hasil = mysqli_fetch_array($query);
+      if ($hasil) {
+         $_SESSION["username"] = $username;
+         header('Location: index.php');
+         exit();
+      } else {
+         echo "Username dan Password Salah";
+      }
    }
-}
 ?>
